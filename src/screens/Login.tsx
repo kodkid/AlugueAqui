032484d139a9341857217b0deec164a8c3628fe0
kodkid <math_moss@hotmail.com>
2024-06-16 17:23:57 -0400

    commit03

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,12 +1,22 @@
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { TextInput } from 'react-native-paper';
+import { StyleSheet, Text, View, Image, } from 'react-native';
+import { TextInput, Card } from 'react-native-paper';
 
-export default function App({navigation}) {
+const Logo = require('../../assets/images/logo.png');
+
+
+
+export default function App({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text>Tela de Login</Text>
-      
+      <View style={styles.imageContainer}>
+        <Image source={Logo} style={styles.logo}></Image>
+        <View style={styles.input}>
+          <TextInput label="E-mail" mode="outlined" />
+          <TextInput label="Senha" mode="outlined" />
+        </View>
+      </View>
+
     </View>
   );
 }
@@ -14,8 +24,24 @@ export default function App({navigation}) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: 'center'
+
+
   },
+  imageContainer: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  logo: {
+    width: 600,
+    height: 600,
+    resizeMode: 'contain',
+  },
+  input: {
+    flex: 1,
+    justifyContent: 'center',
+
+  }
+
 });
