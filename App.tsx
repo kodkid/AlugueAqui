e94a76f53f39bc49f9bea56e8096249432ebbe22
kodkid <math_moss@hotmail.com>
2024-06-16 17:23:57 -0400

    Terceiro Commit

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 
 
 import Login from './src/screens/Login'
 import Register from './src/screens/Register'
 import Home from './src/screens/Home'
-import { fontConfig } from 'react-native-paper/lib/typescript/styles/fonts';
+
 
 const Stack = createNativeStackNavigator();
 export default function App() {
@@ -15,25 +15,25 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen
-         name="Login"
-         component={Login}
-         options={{title: 'Login'}}
-        
+          name="Login"
+          component={Login}
+          options={{ title: 'Login' }}
+
         />
         <Stack.Screen
-         name="Register"
-         component={Register}
-         options={{title: 'Register'}}
-        
+          name="Register"
+          component={Register}
+          options={{ title: 'Register' }}
+
         />
         <Stack.Screen
-         name="Home"
-         component={Home}
-         options={{title: 'Home'}}
-        
+          name="Home"
+          component={Home}
+          options={{ title: 'Home' }}
+
         />
       </Stack.Navigator>
-      </NavigationContainer>
-  
+    </NavigationContainer>
+
   );
 }
