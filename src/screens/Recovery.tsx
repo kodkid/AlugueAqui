16415ec33d87a59c524e910d128907699716a325
kodkid <math_moss@hotmail.com>
2024-06-18 03:17:06 -0400

    Setimo Commit

--- src/screens/Recovery.tsx
+++ src/screens/Recovery.tsx
@@ -1,20 +1,66 @@
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Alert, Text } from 'react-native';
+import { TextInput, Button, } from 'react-native-paper';
+
 
 export default function App({ navigation }) {
     return (
         <View style={styles.container}>
-            <Text>Tela de Recuperação de Conta</Text>
+            <Text style={styles.text}> Recuperação de conta</Text>
+            <TextInput label="Email da sua conta" mode="outlined" />
+            <Button
+                style={styles.buttom}
+                icon="login"
+                mode="elevated"
+                buttonColor="#008080"
+                textColor="#fff"
+                onPress={() => {
+                    Alert.alert(
+                        'Alerta',
+                        'Tem certeza que deseja enviar o email?',
+                        [
+                            { text: 'Cancelar', onPress: () => console.log('Cancel Pressed') },
+                            { text: 'Confirmar', onPress: () => navigation.navigate('Login') }
+                        ],
+                        { cancelable: false }
+                    );
+                }}
+            >
+                Enviar
+            </Button>
+            <Text style={styles.link1} onPress={() => navigation.navigate("Login")}> Voltar</Text>
 
         </View>
+
+
     );
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+        backgroundColor: "#fff",
+        marginHorizontal: 16,
+        justifyContent: "center",
+        gap: 2,
     },
+
+    link1: {
+        color: "#000",
+        alignSelf: 'flex-start',
+
+    },
+
+    buttom: {
+        marginTop: 8,
+
+
+    },
+
+    text: {
+        fontSize: 15,
+        color: "#008080",
+
+    },
+
 });
