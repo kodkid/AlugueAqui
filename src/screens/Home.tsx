682f23051e3ae39d5781d10a29f0ef9855c75dd9
kodkid <math_moss@hotmail.com>
2024-06-18 03:22:08 -0400

    Oitavo Commit

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App({ navigation }) {
+export default function Home({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text>Tela de Login</Text>
+      <Text>Tela da Home</Text>
 
     </View>
   );
