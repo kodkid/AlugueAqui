29ccd6f51d6808ca77b76413975f2b949ef8f211
kodkid <math_moss@hotmail.com>
2024-06-19 20:46:17 -0400

    commit10

--- src/screens/Register.tsx
+++ src/screens/Register.tsx
@@ -1,10 +1,18 @@
 import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { TextInput, Button, } from 'react-native-paper';
 
 export default function Register({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text>Tela de Registro</Text>
+      <Text style={styles.text1}>Cadastro de conta</Text>
+      <View style={styles.registerPanel}>
+      <Text> Nome Completo</Text>
+      <TextInput label="E-mail" mode="outlined" />
+      <TextInput label="Senha" mode="outlined" />
+      
+
+      </View>
 
     </View>
   );
@@ -13,8 +21,53 @@ export default function Register({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    flexDirection: 'column',
+    justifyContent: "center",
+
   },
+
+  registerPanel: {
+    flex: 1,
+    marginTop: 16,
+    gap: 6,
+    paddingHorizontal: 16,
+
+  },
+
+  containerImage: {
+    alignItems: "center",
+
+  },
+
+  containerInput: {
+    flex: 1,
+
+  },
+
+  link1: {
+    color: "#069e6e",
+    alignSelf: "flex-end",
+
+  },
+
+  buttom: {
+    marginTop: 16,
+
+  },
+
+  text1: {
+    fontSize: 24,
+    marginTop: 14,
+    color: "#000",
+
+
+  },
+
+  link2: {
+    color: "#069e6e",
+
+  },
+
+  
 });
