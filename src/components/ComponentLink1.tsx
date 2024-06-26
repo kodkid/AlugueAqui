8a29f887e2a395084f539b1042c0011aeb12e01d
kodkid <math_moss@hotmail.com>
2024-06-26 00:43:48 -0400

    commit17

---
+++ src/components/ComponentLink1.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Text, StyleSheet, View } from "react-native";
+
+interface TextProps {
+  title: string; 
+  onPress: () => void;
+}
+
+const ComponentText: React.FC<TextProps> = ({ title }) => {
+  return (
+    <View>
+      <Text style={styles.text}>{title}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 16,
+    color: "#ffbd59",
+  },
+});
+
+export default ComponentText;
