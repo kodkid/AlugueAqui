8ba9f97b36692f8803af05c45800e2bea0c98c53
ZSILZ <senacsilvio2@gmail.com>
2024-06-30 01:14:52 -0400

    teste

---
+++ src/screens/CardRegister.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function CardRegister({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text> seilaaa</Text>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
