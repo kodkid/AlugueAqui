4f95f159b8f6bc76fc2cfa3a7474d78f32cce85f
kodkid <math_moss@hotmail.com>
2024-06-30 03:05:02 -0400

    Mudanças no CardRegister

--- src/screens/CardRegister.tsx
+++ src/screens/CardRegister.tsx
@@ -1,88 +1,87 @@
 import * as React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { TextInput, Button } from "react-native-paper";
+import { Button } from "react-native-paper";
 import ComponentTextInput from "../components/ComponentTextInput";
 import ComponentTextTitle from "../components/ComponentText";
 
 export default function CardRegister({ navigation }) {
-  return (
-    <View style={styles.container}>
-      <View style={styles.registerPanel}>
-        <ComponentTextTitle title="Numero do " />
-        <ComponentTextInput placeholder="Nome completo" />
-        <ComponentTextTitle title="Digite seu e-mail " />
-        <ComponentTextInput placeholder="e-mail" />
-        <ComponentTextTitle title="Digite sua senha " />
-        <ComponentTextInput placeholder="Senha" />
-        <ComponentTextTitle title="Digite sua senha novamente " />
-        <ComponentTextInput placeholder="Confirmar Senha" />
+    return (
+        <View style={styles.container}>
+            <View style={styles.registerPanel}>
+                <ComponentTextTitle title="Número Do Cartão" />
+                <ComponentTextInput placeholder="Número do Cartão" />
 
-        <Button
-          style={styles.buttom}
-          icon="login"
-          mode="elevated"
-          buttonColor="#ffbd59"
-          textColor="#fff"
-          onPress={() => navigation.navigate("Home")}
-        >
-          Login
-        </Button>
-        <View>
-          <Text style={styles.textPanel}>
-            {" "}
-            Já possui uma conta?{" "}
-            <Text
-              style={styles.link2}
-              onPress={() => navigation.navigate("Login")}
-            >
-              {" "}
-              Logar
-            </Text>
-          </Text>
+                <View style={styles.inlineContainer}>
+                    <View style={styles.inlineInput}>
+                        <ComponentTextTitle title="Validade" />
+                        <ComponentTextInput style={styles.smallInput} placeholder="MM/AA" />
+                    </View>
+                    <View style={styles.inlineInput}>
+                        <ComponentTextTitle title="CVC" />
+                        <ComponentTextInput style={styles.smallInput} placeholder="CVC" />
+                    </View>
+                </View>
+
+                <ComponentTextTitle title="Titular do cartão" />
+                <ComponentTextInput placeholder="Titular do cartão" />
+                <ComponentTextTitle title="CPF do titular" />
+                <ComponentTextInput placeholder="CPF do titular" />
+
+                <Button
+                    style={styles.button}
+                    icon="login"
+                    mode="elevated"
+
+                    buttonColor="#ffbd59"
+
+                    textColor="#fff"
+                    onPress={() => navigation.navigate("Home")}
+                >
+                    Registrar
+                </Button>
+                <View style={styles.textPanel}>
+                    <Text>
+                        Já possui uma conta?{" "}
+                        <Text style={styles.link} onPress={() => navigation.navigate("Login")}>
+                            Logar
+                        </Text>
+                    </Text>
+                </View>
+            </View>
         </View>
-      </View>
-    </View>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    flexDirection: "column",
-    justifyContent: "center",
-  },
-
-  registerPanel: {
-    flex: 1,
-    marginTop: 16,
-    gap: 6,
-    paddingHorizontal: 16,
-  },
-
-  containerImage: {
-    alignItems: "center",
-  },
-
-  containerInput: {
-    flex: 1,
-  },
-
-  link1: {
-    color: "#000",
-    alignSelf: "flex-start",
-  },
-
-  buttom: {
-    marginTop: 8,
-  },
-
-
-  link2: {
-    color: "#fbd796",
-  },
-  textPanel: {
-    alignSelf: "flex-start",
-    padding: 4,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        justifyContent: "center",
+        paddingHorizontal: 16,
+    },
+    registerPanel: {
+        marginTop: 16,
+    },
+    inlineContainer: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginBottom: 10,
+    },
+    inlineInput: {
+        flex: 1,
+        marginRight: 10,
+    },
+    smallInput: {
+        width: "100%",
+    },
+    button: {
+        marginTop: 16,
+    },
+    textPanel: {
+        alignSelf: "flex-start",
+        marginTop: 16,
+    },
+    link: {
+        color: "#fbd796",
+    },
 });
