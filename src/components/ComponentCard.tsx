ddabdd7b60c02e182b8a09e2eb91bea03d81aec3
kodkid <math_moss@hotmail.com>
2024-07-06 16:06:23 -0400

    Mudanças no card

--- src/components/ComponentCard.tsx
+++ src/components/ComponentCard.tsx
@@ -6,7 +6,6 @@ const ShopItem1 = require("../../assets/images/shop-item-1.png");
 const ComponentCard = () => (
   <Card>
     <Card.Actions>
-    <Card.Cover source={{}} />
       <Button>Cancel</Button>
       <Button>Ok</Button>
     </Card.Actions>
