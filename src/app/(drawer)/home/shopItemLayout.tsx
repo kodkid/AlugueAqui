66a229b4405a036c8d7cbf31196432b2bcae8e95
kodkid <math_moss@hotmail.com>
2024-07-19 16:54:39 -0400

    arrumando erros

--- src/app/(drawer)/home/shopItemLayout.tsx
+++ src/app/(drawer)/home/shopItemLayout.tsx
@@ -1,19 +1,16 @@
 import React from "react";
 import {
-  Image,
   View,
   StyleSheet,
-  TouchableOpacity,
   Dimensions,
 } from "react-native";
 import { Drawer } from "expo-router/drawer";
 import { DrawerToggleButton } from "@react-navigation/drawer";
-import { Button } from "react-native-paper";
-import { AntDesign } from "@expo/vector-icons";
-import { router } from "expo-router";
+
 
 import ComponentCardShop from "../../../components/ComponentCardShop1";
 import ComponentCardBox2 from "../../../components/ComponentCardBox2";
+import ComponentCardBox3 from "../../../components/ComponentCardBox3";
 
 const ShopItem1 = require("../../../../assets/icon.png");
 
@@ -33,8 +30,7 @@ export default function ShopItemLayout() {
         <ComponentCardBox2 />
       </View>
       {/* Fim do componentCardBox2 */}
-      <View style={styles.containerButtom}>
-      </View>
+     
     </View>
   );
 }
@@ -45,24 +41,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     gap: 8,
   },
-  containerImage: {
-    marginTop: 16, 
-    alignItems: "center",
-  },
-  shopItem: {
-    height: 200,
-    width: 200,
-    resizeMode: "contain",
-  },
-  navigationLeft: {
-    position: "absolute",
-    left: 16,
-    top: 16,
-    justifyContent: "center",
-    alignItems: "center",
-    zIndex: 1, 
-    marginTop: 64,
-  },
+
   navigationRight: {
     position: "absolute",
     right: 16,
@@ -75,17 +54,5 @@ const styles = StyleSheet.create({
   componentCardBox2: {
     height: "30%",
   },
-  containerButtom: {
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  buttom1: {
-    marginTop: 8,
-    width: "90%",
-  },
-  buttom2: {
-    marginTop: 8,
-    width: "90%",
-    borderColor: "#FFB800",
-  },
+
 });
